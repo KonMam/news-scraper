de3ff281b31302f1a93a705498bb7afaf2ecdb0f
KonMam <k.mamonas@gmail.com>
2022-12-28 17:18:24 +0200

    write to file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
 import json
 
 URL = "https://www.vz.lt/visos-naujienos"
@@ -9,32 +8,20 @@ data = {}
 
 r = requests.get(url=URL)
 
-# soup = BeautifulSoup(r.text, 'html.parser')
+soup = BeautifulSoup(r.text, 'html.parser')
+articles = soup.select('div.one-article div.txt-wr a')
 
-html_text = open('demo.txt', 'r', encoding='utf-8')
-soup = BeautifulSoup(html_text, 'html.parser')
-
-# articles = soup.find_all(
-#    "div",
-#    class_="one-article"
-# )
-
-# something = ""
-
-# for article in articles
-#    something += article.get_text().strip()
-
-filtered = soup.select_one('div.one-article div.txt-wr a')
-
-if filtered:
-    data[0] = {
-        'title': str(filtered.get('title')),
-        'href': str(filtered.get('href'))
+i = 0
+for article in articles:
+    data[i] = {
+        'title': str(article.get('title')),
+        'href': str(article.get('href'))
     }
+    i += 1
 
 formatted_json = json.dumps(data, indent=4, ensure_ascii=False)
 
 if data:
-    f = open('results.json', "a", encoding='utf-8')
+    f = open('results.json', "w", encoding='utf-8')
     f.write(formatted_json)
     f.close()
